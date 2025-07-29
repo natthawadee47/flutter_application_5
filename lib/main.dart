1d7c1684d93f802968b046d2d64958d7544b93f5
natthawadee47 <natthawadeec66@nu.ac.th>
2025-07-29 10:06:14 +0700

    add container image

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,62 @@
 import 'package:flutter/material.dart'; // Import ไลบรารี Material ของ Flutter
- 
+
 void main() {
   // จุดเริ่มต้นของแอป Flutter
   runApp(const MyApp()); // บอก Flutter ว่าวิดเจ็ตใดคือรากฐานของแอป
 }
- 
+
 // MyApp โดยทั่วไปคือวิดเจ็ตรากฐานของแอปพลิเคชันของคุณ
 class MyApp extends StatelessWidget {
   const MyApp({super.key}); // Constructor
- 
+
   @override
   Widget build(BuildContext context) {
     // เมธอด build() ส่งคืนโครงสร้างต้นไม้วิดเจ็ตสำหรับวิดเจ็ตนี้
     return MaterialApp(
+      title: 'My Awesome App',
+      debugShowCheckedModeBanner: false, // ซ่อนแบนเนอร์ debug
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 76, 179, 127),
-        ),
-        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true, // ใช้ Material Design เวอร์ชันล่าสุด
       ),
-      title: 'Flutter Demo',
-      home: const MyHomePage(),
+      home: const MyHomePage(), // วิดเจ็ตหน้าจอหลักของคุณ
     );
   }
 }
- 
+
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
- 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       // ให้โครงสร้างแอปพื้นฐาน
       appBar: AppBar(
-        title: const Text('Chapter 3 Lab.'),
-        backgroundColor: Colors.teal, // ปรับแต่งสี AppBar
+        title: const Text('Home Page'),
+        backgroundColor: Colors.blue, // ปรับแต่งสี AppBar
       ),
       body: Center(
+        // พื้นที่เนื้อหาหลัก
         child: Container(
-        padding: EdgeInsets.all(50.0),
+          padding: EdgeInsets.all(10.0),
           width: 300.0,
-          height: 200.0,
+          height: 150.0,
           decoration: BoxDecoration(
             color: Colors.amber,
-            borderRadius: BorderRadius.all(Radius.circular(8.0))
-          ),
-          child: const Text('inside container',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 20.0,
-              color: Colors.brown,
- 
+            borderRadius: BorderRadius.horizontal(
+              left: Radius.circular(10.0),
+              right: Radius.elliptical(50.0, 10),
             ),
-         
+          ),
+          child: Center(
+            child: Image.asset(
+              'assets/image/cat.jpg',
+              width: 150,
+              fit: BoxFit.contain,
+            ),
           ),
         ),
       ),
- 
- 
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           // การกระทำที่จะทำเมื่อกดปุ่ม
